120e5cc685d2be7f72c3a68996c39036820a9190
NadaBargougui <nadabargougui1@gmail.com>
2026-03-30 01:46:30 +0100

    rbac

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -21,7 +21,6 @@ data "aws_eks_cluster" "eks" {
   name = module.eks.eks_cluster_name
 }
 
-# This is to authenticate helm to interact with Kubernetes API
 provider "helm" {
   kubernetes = {          
     host                   = data.aws_eks_cluster.eks.endpoint  
@@ -33,26 +32,3 @@ provider "helm" {
     }
   }
 }
-
-# When Terraform runs a helm_release:
-# 1. Terraform runs: "aws eks get-token"
-# 2. AWS STS (Security Token Service) verifies that terraform is allowed to assume an IAM role, and then gives temporary credentials for that role
-# 3. Terraform sends request to Kubernetes API using the temporary token
-# 4. Kubernetes:
-#      - validates that tokens was issued by AWS STS
-#      - uses a special ConfigMap called "aws-auth" in the "kube-system" namespace to map AWS IAM identities (users/roles) to Kubernetes usernames and groups (bridges AWS IAM → Kubernetes RBAC)
-# --> Terraform can now do Helm installs/updates charts inside the cluster
-
-# Besides that:
-# We are supposed to create an IAM role for Terraform with "eks:GetToken" permissions
-# and edit the aws-auth ConfigMap to map the role we created to a Kubernetes username and groups.
-# (e.g., username "terraform" and groups "system:masters" for admin permissions)
-# Or, for more restricted access, we can create a custom RBAC role with specific permissions.
-
-# In our case:
-# - We don’t need to create a separate IAM role or modify aws-auth,
-#   because Terraform is using an IAM user (SSO profile) that already has admin permissions.
-# - Furthermore, this is the same IAM user that was used to create the EKS cluster.
-#   EKS automatically adds the creator identity to the aws-auth ConfigMap in the "kube-system"
-#   namespace with groups: "system:masters", giving full admin privileges in Kubernetes.
-# - Therefore, Terraform can authenticate and perform Helm installs/updates without any additional setup.
