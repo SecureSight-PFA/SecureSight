ee7eff60c788e1cd446d269f7009c1723622ec6f
Amal Sboui <amal.sboui@insat.ucar.tn>
2026-04-07 14:55:09 +0100

    routes

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -17,3 +17,14 @@ module "subnets" {
   private_subnet_cidrs = var.private_subnet_cidrs
 
 }
+
+module "routes" {
+  source = "../../modules/routes"
+
+  vpc_id              = module.vpc.vpc_id
+  internet_gateway_id = module.vpc.internet_gateway_id
+  public_subnet_ids   = module.subnets.public_subnet_ids
+  private_subnet_ids  = module.subnets.private_subnet_ids
+  nat_gateway_ids     = module.subnets.nat_gateway_ids
+  environment         = var.environment
+}
---
+++ terraform/modules/routes/main.tf
@@ -0,0 +1,44 @@
+# --- Public route table, routes internet traffic through igw ---
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.internet_gateway_id
+  }
+
+  tags = {
+    Name        = "${var.environment}-public-rt"
+  }
+}
+
+#Associate the public subnets with public route table
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_ids)
+
+  subnet_id      = var.public_subnet_ids[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+# --- Private route tables, one per AZ, routes through nat gw ---
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_ids)
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = var.nat_gateway_ids[count.index]
+  }
+
+  tags = {
+    Name        = "${var.environment}-private-rt-${count.index}"
+  }
+}
+
+# Associate private subnets with their private route tables
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_ids)
+
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private[count.index].id
+}
